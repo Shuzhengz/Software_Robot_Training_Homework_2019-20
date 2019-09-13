43110cb3bc041c7da762d0f5d093df7ecc281682
Tom_Zhang <treez.zhang@gmail.com>
2019-09-12 20:05:30 -0700

    Update the Example HeloWorld File

--- Example/HelloWorld.java
+++ Example/HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld{
 
      public static void main(String []args){
-        System.out.println("Hello World");
+      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("Enter a number");
+      String num = myObj.nextLine();  // Read user input
+      System.out.println("Hello World" + num);
      }
 }
